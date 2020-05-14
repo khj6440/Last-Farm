1e79f8e33551332dbd1f11c10147bd6cca4e73be
helloimsh2 <pspp1024@gmail.com>
2020-05-14 11:04:02 +0900

    JDBCTemplate수정

--- src/common/JDBCTemplate.java
+++ src/common/JDBCTemplate.java
@@ -7,170 +7,85 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JDBCTemplate {
-<<<<<<< HEAD
-<<<<<<< HEAD
-   
-   public static Connection getConnection() {
-      
-      Connection conn = null;
-      
-      try {
-         Class.forName("oracle.jdbc.driver.OracleDriver");
-         
-         conn= DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","KH_SERVER","1234");
-         
-         conn.setAutoCommit(false); // 오토커밋 방지 설정
-         
-      } catch (ClassNotFoundException e) {
-         e.printStackTrace();
-      } catch (SQLException e) {
-         e.printStackTrace();
-      }
-      
-      
-      return conn;
-   }
-   
-   public static void close(Connection conn) {
-      
-      try {
-         if(conn != null && !conn.isClosed()) {
-            conn.close();
-         }
-         
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-   
-   public static void close(ResultSet rset) {
-      try {
-         if(rset != null && !rset.isClosed()) {
-            rset.close();
-         }
-         
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-   
-   public static void close(Statement stmt) {
-      
-      try {
-         if(stmt!=null && !stmt.isClosed()) {
-            stmt.close();
-         }
-         
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-   }
-
-   public static void commit(Connection conn) {
-      try {
-         
-         if(conn!=null && !conn.isClosed()) {
-            conn.commit();
-         }
-         
-      } catch (Exception e) {
-          e.printStackTrace();
-      }
-   }
-   
-   public static void rollback(Connection conn) {
-      
-      try {
-         
-         if(conn!=null && !conn.isClosed()) {
-            conn.rollback();
-         }
-         
-      } catch (Exception e) {
-          e.printStackTrace();
-      }
-   }
-   
-}
-=======
-	
-	public static Connection getConnection() {
-=======
->>>>>>> c206ee5a196b89bdf42c692a558bc9d5a8c8dd7b
 
 	public static Connection getConnection() {
+
 		Connection conn = null;
-		try {
-			// 1.드라이버 등록
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "lastFarm", "1234");
-			// 자동 커밋 방지 설정
 
-			conn.setAutoCommit(false);
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe", "KH_SERVER", "1234");
+
+			conn.setAutoCommit(false); // 오토커밋 방지 설정
+
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return conn;
 
-		
+		return conn;
 	}
-	
+
 	public static void close(Connection conn) {
+
 		try {
-			if(conn != null && !conn.isClosed()) {
+			if (conn != null && !conn.isClosed()) {
 				conn.close();
 			}
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 	public static void close(ResultSet rset) {
 		try {
-			if(rset != null && !rset.isClosed()) {
+			if (rset != null && !rset.isClosed()) {
 				rset.close();
 			}
-		}catch (Exception e ) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void close(Statement stmt) {
+
 		try {
-			if(stmt != null && !stmt.isClosed()) {
+			if (stmt != null && !stmt.isClosed()) {
 				stmt.close();
 			}
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void commit(Connection conn) {
 		try {
-			if(conn!=null && !conn.isClosed()) {
+
+			if (conn != null && !conn.isClosed()) {
 				conn.commit();
 			}
-		}catch (Exception e ) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void rollback(Connection conn) {
+
 		try {
-			if(conn!=null && !conn.isClosed()) {
-				
+
+			if (conn != null && !conn.isClosed()) {
 				conn.rollback();
 			}
-		}catch (Exception e ) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
 }
->>>>>>> 8fc76aa5483c8e188ec8b28549477f3eb7468d44
